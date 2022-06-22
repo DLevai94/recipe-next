cd1ef1f4f3823621c9e0d9fa12cd7a24f981afc4
Roland Levai <rolandlevai99@gmail.com>
2022-06-22 11:36:37 +0100

    There's now an index page. ish.

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,16 @@ export default function Home() {
         <title>{`Recipes :) Nextjs`}</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      HELOBELO
+      <div className="container mx-auto w-screen py-10 px-5 md:px-10 h-full flex flex-col justify-center">
+        <h2 className="py-10 text-3xl font-semibold uppercase">
+          All the recipes you'll <br />
+          ever need. <br />
+          In one place
+        </h2>
+        <button className="px-5 py-2 w-52 bg-fuchsia-500 border-4 border-black text-xl font-bold uppercase">
+          Jump right in
+        </button>
+      </div>
     </>
   )
 }
