f438d55ff5c5806e547aa19aeddbe5f9a3a33de0
Roland Levai <rolandlevai99@gmail.com>
2022-06-22 21:21:05 +0100

    Changes

--- pages/about.js
+++ pages/about.js
@@ -2,7 +2,7 @@ const About = ({ description }) => {
   return <p className="text-center">{description}</p>
 }
 
-export async function getStaticProps(context) {
+export async function getStaticProps() {
   // fetch data from local json file
   const data = require(`../data/about/about-content.json`)
   return {
