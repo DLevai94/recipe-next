81864828f23a9521336aa215006eeac7d72e4f2a
Roland Levai <rolandlevai99@gmail.com>
2022-07-06 14:47:38 +0100

    added dot. kek

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
         <h2 className="py-10 text-3xl font-semibold uppercase">
           All the recipes you&apos;ll <br />
           ever need. <br />
-          In one place
+          In one place.
         </h2>
         <button className="px-5 py-2 w-52 bg-fuchsia-500 border-4 border-black text-xl font-bold uppercase">
           Jump right in
